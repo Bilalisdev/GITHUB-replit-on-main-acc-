3e8e721fa045f2f53b73adc340e340a30e890cea
Bilalisdev <122781871+Bilalisdev@users.noreply.github.com>
2023-01-18 15:11:29 +0500

    Add files via upload
    another programs

---
+++ new/telltemprtureincelcius.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ int celcius;
+  int farenheit;
+
+// ask for the celcius 
+  cout<<"enter the celcius ";
+  cin>>celcius;
+
+//enter the formula
+
+ farenheit=(celcius*9/5)+32;
+
+   cout<< "the temprature in farenheit is";
+  cout<<farenheit;
+  
+}
